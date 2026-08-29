05d3250220b549ca3302b981681478209dea866d
PrannavP <ramsarilekhi@gmail.com>
2026-08-29 14:21:44 +0545

    Change user admin data seed in table, add encrpyt hash password is Admin@12.

--- scripts/10 SEED/0001_app.seed_user.sql
+++ scripts/10 SEED/0001_app.seed_user.sql
@@ -5,4 +5,4 @@
 
 INSERT INTO app.tbl_user (username, email, password_hash, full_name, phone, is_active, is_deleted, created_by, updated_by)
 VALUES 
-('admin', 'admin@admin.com', 'admin', 'Admin Admin', '1234567890', true, false, 0, 0)
+('admin', 'admin@admin.com', '$2b$10$A3QZ4rVYLiVPfneWWFXwRePf4QHBGzGSBJraB6C5dut81ZakVC6mK', 'Admin Admin', '1234567890', true, false, 0, 0)
